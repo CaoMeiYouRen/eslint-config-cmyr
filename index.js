6de0e843948ba930aea6483c427c902c568eba0d
草梅友仁 <996881204@qq.com>
2020-09-16 22:11:47 +0800

    修复冲突规则

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ module.exports = {
         '@typescript-eslint/await-thenable': [2], // 不允许等待一个非Thhenable的值
         '@typescript-eslint/comma-spacing': [2], // 在逗号前后强制使用一致的空格。
         '@typescript-eslint/explicit-function-return-type': [0], // 要求函数和类方法的显式返回类型
+        '@typescript-eslint/explicit-module-boundary-types': [0], // 要求导出函数和类的公共类方法的显式返回和参数类型
         '@typescript-eslint/indent': [2, 4, { SwitchCase: 1 }], // 强制使用一致的缩进// case 子句将相对于 switch 语句缩进 4 个空格，即一个tab
         '@typescript-eslint/member-delimiter-style': [2, { // 接口和类型定义中禁用不必要的分号(typescript扩展支持)
             multiline: {
@@ -47,10 +48,18 @@ module.exports = {
         '@typescript-eslint/no-extra-parens': [IS_PROD ? 2 : 0], // 不允许不必要的括号
         '@typescript-eslint/no-empty-function': [IS_PROD ? 2 : 0], // 禁止空函数
         '@typescript-eslint/no-empty-interface': [0], // 不允许声明空接口
-        '@typescript-eslint/no-unused-vars': [IS_PROD ? 2 : 0], // 禁止未使用的变量
+        '@typescript-eslint/no-floating-promises': [2, { ignoreIIFE: true }], // 需要适当地处理类似 promise 的值
+        '@typescript-eslint/no-unused-vars': [0], // 禁止未使用的变量
+        '@typescript-eslint/no-unsafe-assignment': [0], // 不允许将变量和属性分配给 any
+        '@typescript-eslint/no-unsafe-call': [0], // 不允许调用 any
+        '@typescript-eslint/no-unsafe-member-access': [0], // 不允许成员访问 any 类型变量
+        '@typescript-eslint/no-unsafe-return': [0], // 不允许从函数返回 any
+        '@typescript-eslint/prefer-regexp-exec': [0], // 强制执行RegExp#exec被使用，而不是String#match如果没有提供全局标志
         '@typescript-eslint/quotes': [2, 'single'], // 强制使用前后一致的双引号、双引号或单引号
         '@typescript-eslint/require-await': [IS_PROD ? 2 : 0], // 不允许没有异步函数的异步函数await表达
         '@typescript-eslint/return-await': [IS_PROD ? 2 : 0, 'never'], // 禁用不必要的 return await
+        '@typescript-eslint/restrict-template-expressions': [0], // 强制模板文字表达式为字符串类型
+        '@typescript-eslint/restrict-plus-operands': [0], // 当添加两个变量时，操作数必须类型为数字或类型为字符串
         '@typescript-eslint/semi': [2, 'never'], // 禁用不必要的分号(typescript扩展支持)
         '@typescript-eslint/type-annotation-spacing': [2], // 需要在类型注释前后保持一致的间距。
         'arrow-body-style': [IS_PROD ? 2 : 0, 'as-needed'], // 要求箭头函数体使用大括号,当大括号是可以省略的，强制不使用它们 (默认)
@@ -95,10 +104,10 @@ module.exports = {
         'no-shadow': [2], // 禁止变量声明与外层作用域的变量同名
         'no-trailing-spaces': [0], // 禁用行尾空格
         'no-unneeded-ternary': [2], // 禁止可以在有更简单的可替代的表达式时使用三元操作符
-        'no-unused-vars': [IS_PROD ? 2 : 0], // 禁止出现未使用过的变量
+        'no-unused-vars': [0], // 禁止出现未使用过的变量
         'no-use-before-define': [0], // 禁止在变量定义之前使用它们
         'no-useless-constructor': [IS_PROD ? 2 : 0], // 禁用不必要的构造函数
-        'no-useless-return': IS_PROD ? 2 : 0, // 禁止多余的 return 语句
+        'no-useless-return': [IS_PROD ? 2 : 0], // 禁止多余的 return 语句
         'no-var': [2], // 要求使用 let 或 const 而不是 var
         'object-shorthand': [2], // 要求或禁止对象字面量中方法和属性使用简写语法
         'operator-assignment': [2], // 要求或禁止尽可能地简化赋值操作
@@ -108,7 +117,7 @@ module.exports = {
         'prefer-template': [2], // 建议使用模板字面量而非字符串连接 (prefer-template)
         'quote-props': [2, 'as-needed', { keywords: false, numbers: true }], // 当没有严格要求时，禁止对象字面量属性名称使用引号
         quotes: [2, 'single'], // 强制使用一致的反勾号、双引号或单引号double
-        'require-await': [2], // 不允许没有异步函数的异步函数await表达
+        'require-await': [0], // 不允许没有异步函数的异步函数await表达
         semi: [2, 'never'], // 要求或禁止使用分号代替 ASI
         'semi-style': [2, 'last'], // 强制分号出现在句子末尾
         'sort-imports': [0], // import 排序

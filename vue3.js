e99b6866319e3b29f20bcb71af994a06e4e463f6
CaoMeiYouRen <996881204@qq.com>
2020-10-31 17:06:42 +0800

    fix: 修复错误

--- vue3.js
+++ vue3.js
@@ -2,7 +2,7 @@
 const IS_PROD = process.env.NODE_ENV === 'production' ? 2 : 0
 module.exports = {
     extends: [
-        'plugin:vue3-recommended',
+        'plugin:vue/vue3-recommended',
         require.resolve('./vue'),
     ],
     rules: {

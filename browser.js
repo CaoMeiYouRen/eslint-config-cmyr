53ec09a71b03b88bb6fd295d4a5667d37c87fe50
CaoMeiYouRen <996881204@qq.com>
2020-12-29 20:22:15 +0800

    fix: 修复 browser.js bug

--- browser.js
+++ browser.js
@@ -1,5 +1,7 @@
 const __ERROR__ = process.env.NODE_ENV === 'production' ? 2 : 0
 const __WARN__ = process.env.NODE_ENV === 'production' ? 1 : 0
 module.exports = {
-    'no-console': [__ERROR__, { allow: ['warn', 'error'] }], // 禁止console
+    rules: {
+        'no-console': [__ERROR__, { allow: ['warn', 'error'] }], // 禁止console
+    },
 }

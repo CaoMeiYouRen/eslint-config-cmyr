633993c53b7e751a87c27856eae0057e093e49de
CaoMeiYouRen <996881204@qq.com>
2021-02-27 19:35:50 +0800

    fix: 修复 typescript 下 no-shadow bug；新增规则

--- index.js
+++ index.js
@@ -31,14 +31,20 @@ module.exports = {
     },
     parser: '@typescript-eslint/parser',
     rules: {
+        indent: [0], // 强制使用一致的缩进
+        'no-shadow': [0], // 禁止变量声明与外层作用域的变量同名
         // '@typescript-eslint/await-thenable': [2], // 不允许等待一个非Thhenable的值
+        '@typescript-eslint/brace-style': [2, 'always'], // 禁止或强制在代码块中开括号前和闭括号后有空格
+        '@typescript-eslint/comma-dangle': [2, 'always-multiline'], // 要求或禁止使用拖尾逗号
+        '@typescript-eslint/comma-spacing': [2, { before: false, after: true }], // 强制在逗号周围使用空格
         '@typescript-eslint/camelcase': [0], // 驼峰式风格
-        '@typescript-eslint/comma-spacing': [2], // 在逗号前后强制使用一致的空格。
+        '@typescript-eslint/default-param-last': [2], // 最后执行缺省参数
         '@typescript-eslint/explicit-function-return-type': [0], // 要求函数和类方法的显式返回类型
         '@typescript-eslint/explicit-module-boundary-types': [0, {
             allowArgumentsExplicitlyTypedAsAny: true,
         }], // 要求导出函数和类的公共类方法的显式返回和参数类型
         '@typescript-eslint/indent': [2, 4, { SwitchCase: 1 }], // 强制使用一致的缩进// case 子句将相对于 switch 语句缩进 4 个空格，即一个tab
+        '@typescript-eslint/keyword-spacing': [2, { before: true, after: true }], // 强制关键字周围空格的一致性
         '@typescript-eslint/member-delimiter-style': [2, { // 接口和类型定义中禁用不必要的分号(typescript扩展支持)
             multiline: {
                 delimiter: 'none', // 多行类型定义不使用分隔符
@@ -54,10 +60,13 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': [0], // 不允许使用any类型
         '@typescript-eslint/no-extra-parens': [__ERROR__, 'all', { enforceForArrowConditionals: false }], // 不允许不必要的括号
         '@typescript-eslint/no-inferrable-types': [0], // 对于初始化为数字、字符串或布尔值的变量或参数，不允许显式类型声明
+        '@typescript-eslint/no-shadow': [2], // 禁止变量声明与外层作用域的变量同名
         '@typescript-eslint/no-unused-vars': [0], // 禁止未使用的变量
+        '@typescript-eslint/object-curly-spacing': [2, 'always'], // 强制在花括号中使用一致的空格
         '@typescript-eslint/quotes': [2, 'single'], // 强制使用前后一致的双引号、双引号或单引号
         '@typescript-eslint/semi': [2, 'never'], // 禁用不必要的分号(typescript扩展支持)
         '@typescript-eslint/type-annotation-spacing': [2], // 需要在类型注释前后保持一致的间距。
+        '@typescript-eslint/space-infix-ops': [2], // 要求操作符周围有空格
         // '@typescript-eslint/no-floating-promises': [0, { ignoreIIFE: true }], // 需要适当地处理类似 promise 的值
         // '@typescript-eslint/no-misused-promises':[0],// 避免在非 promise 的地方使用 promise
         // '@typescript-eslint/no-unsafe-assignment': [0], // 不允许将变量和属性分配给 any
@@ -69,6 +78,6 @@ module.exports = {
         // '@typescript-eslint/return-await': [__ERROR__, 'never'], // 禁用不必要的 return await
         // '@typescript-eslint/restrict-template-expressions': [0], // 强制模板文字表达式为字符串类型
         // '@typescript-eslint/restrict-plus-operands': [0], // 当添加两个变量时，操作数必须类型为数字或类型为字符串
-        indent: [0], // 强制使用一致的缩进
+
     },
 }

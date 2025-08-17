58da0b19384253f3baf961e9b06581f574e028c0
CaoMeiYouRen <caomeiyouren@qq.com>
2025-08-17 19:53:03 +0800

    feat: 更新 Nuxt.js ESLint 配置，新增支持的文件类型，并替换测试文件

--- nuxt.js
+++ nuxt.js
@@ -2,9 +2,27 @@ import { createConfigForNuxt, defineFlatConfigs } from '@nuxt/eslint-config'
 import { defineConfig } from 'eslint/config'
 import vueConfig from './vue.js'
 // Nuxt.js 规则
-export default defineFlatConfigs({
-    // features: {
-    //     stylistic: true, // 启用样式化规则
-    //     typescript: true, // 启用 TypeScript 支持
-    // },
-}).append(vueConfig)
+export default defineConfig([
+    vueConfig,
+    {
+        files: ['**/*.{js,cjs,mjs,jsx,ts,tsx,mts,cts,vue}'],
+        languageOptions: {
+            globals: {
+                useFetch: false,
+                useAsyncData: false,
+                useCookie: false,
+                useError: false,
+                useHead: false,
+                useLazyAsyncData: false,
+                useLazyFetch: false,
+                useNuxtApp: false,
+                useNuxtData: false,
+                useRoute: false,
+                useRouter: false,
+                useState: false,
+                useRuntimeConfig: false,
+                useSeoMeta: false,
+            },
+        },
+    },
+])
---
+++ test/NuxtTest.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>Hello vue.test.vue</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig()
+</script>
+
+<style lang="scss" scoped>
+</style>

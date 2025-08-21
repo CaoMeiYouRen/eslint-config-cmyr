d5a172d91f591badc551a0f2aad07a2fe6ac095d
CaoMeiYouRen <caomeiyouren@qq.com>
2025-08-22 00:06:51 +0800

    chore: 删除不再使用的 ESLint 配置文件和工具函数

--- _browser.js
+++
@@ -1,6 +0,0 @@
-const __ERROR__ = process.env.NODE_ENV === 'production' ? 2 : 0
-module.exports = {
-    rules: {
-        'no-console': [__ERROR__, { allow: ['warn', 'error'] }], // 禁止console
-    },
-}

37c994e4da7b46a0f7d4ed9dc8decf598dab068f
CaoMeiYouRen <caomeiyouren@qq.com>
2026-04-02 22:00:31 +0800

    fix(strict): 更新 TypeScript 规则配置
    - 修改 '@typescript-eslint/only-throw-error' 规则描述
    - 添加多个 TypeScript 规则以增强代码质量

--- strict.js
+++ strict.js
@@ -31,11 +31,11 @@ export default defineConfig([
             '@typescript-eslint/no-unnecessary-type-assertion': [0], // 禁止不必要的类型断言
             '@typescript-eslint/no-unsafe-enum-comparison': [1], // 禁止将枚举与非枚举类型进行比较
             '@typescript-eslint/no-redundant-type-constituents': [1], // 禁止联合类型中包含冗余的成员
-            '@typescript-eslint/only-throw-error': [1], // 禁止抛出非 Error 类型的值
+            '@typescript-eslint/only-throw-error': [1], // 禁止不做任何处理就再次向上抛出相同的 error
             '@typescript-eslint/prefer-optional-chain': [1], // 建议使用可选链 (?.) 替代逻辑与 (&&) 来访问深层嵌套的属性
             '@typescript-eslint/require-await': [1], // 禁止在 async 函数中不使用 await 表达式
             '@typescript-eslint/non-nullable-type-assertion-style': [0], // 建议使用非空断言 (postfix !) 替代类型断言来消除 null 和 undefined
-
+            '@typescript-eslint/no-inferrable-types': [0], // 对于初始化为数字、字符串或布尔值的变量或参数，不允许显式类型声明
             '@typescript-eslint/explicit-function-return-type': [0], // 要求函数和类方法的显式返回类型
             '@typescript-eslint/explicit-module-boundary-types': [1, {
                 allowArgumentsExplicitlyTypedAsAny: true,
@@ -49,8 +49,13 @@ export default defineConfig([
             '@typescript-eslint/unbound-method': [1], // 不允许不绑定上下文的类方法引用
             '@typescript-eslint/restrict-template-expressions': [0], // 允许在模板字符串中使用非字符串类型的表达式
             '@typescript-eslint/prefer-nullish-coalescing': [1], // 建议使用空值合并运算符 (??) 替代逻辑或 (||) 来处理 null 或 undefined
-
-
+            '@typescript-eslint/no-unnecessary-boolean-literal-compare': [1], // 禁止与 boolean 字面量进行不必要的比较
+            '@typescript-eslint/return-await': [1], // 禁止在返回语句中使用 await，除非在 try/catch 块中
+            '@typescript-eslint/no-invalid-void-type': [1], // 禁止在泛型或返回类型之外使用 void 类型
+            '@typescript-eslint/no-unnecessary-type-parameters': [1], // 禁止在类型参数未被使用时将其添加到泛型函数中
+            '@typescript-eslint/no-misused-spread': [0], // 禁止在可能引起意外行为时使用展开运算符
+            '@typescript-eslint/no-dynamic-delete': [0], // 允许使用 delete 操作符删除对象的属性，即使该对象的类型不包含索引签名
+            '@typescript-eslint/no-unnecessary-type-conversion': [0], // 禁止在表达式类型或值未发生变化时使用转换惯用法
         },
     },
 ])
